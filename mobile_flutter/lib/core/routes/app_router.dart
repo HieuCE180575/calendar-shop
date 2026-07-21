939a44f1a5bb830a5d10097b99b641b28e1e47a8
Thạch Ngọc Trí <thachngoctritc2004@gmail.com>
2026-07-21 11:14:15 +0700

    fix: resolve duplicate admin_order_list_page and update db connection string

--- mobile_flutter/lib/core/routes/app_router.dart
+++ mobile_flutter/lib/core/routes/app_router.dart
@@ -11,6 +11,7 @@ import '../../features/admin/presentation/pages/orders/admin_order_list_page.dar
 import '../../features/admin/presentation/pages/admin_discount_form_page.dart';
 import '../../features/admin/presentation/pages/admin_discount_list_page.dart';
 import '../../features/admin/presentation/pages/admin_discount_detail_page.dart';
+import '../../features/admin/presentation/pages/admin_home_page.dart';
 import '../../features/admin/presentation/pages/admin_statistics_page.dart';
 import '../../features/admin/presentation/pages/admin_user_detail_page.dart';
 import '../../features/admin/presentation/pages/admin_user_list_page.dart';
